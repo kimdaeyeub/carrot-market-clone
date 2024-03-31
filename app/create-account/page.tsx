527b5d17943ea441e0ff83ed8e2ffa314f013e1e
kimdaeyeub <kdy3915@naver.com>
2024-04-01 03:01:36 +0900

    Create Account Screen

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,23 +1,34 @@
+import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 
 export default function CreateAccount() {
   return (
-    <div>
-      <div>
-        <h1>안녕하세요!</h1>
-        <h2>Fill in the form below to join!</h2>
+    <div className="flex flex-col gap-10 py-8 px-6">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h1 className="text-2xl">안녕하세요!</h1>
+        <h2 className="text-xl">Fill in the form below to join!</h2>
       </div>
-      <form>
-        <div>
-          <input type="text" placeholder="Username" required />
-          <span>Input Error</span>
+      <form className="flex flex-col gap-3">
+        <div className="flex flex-col gap-2">
+          <input
+            className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-1 focus:ring-2 ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400"
+            type="text"
+            placeholder="Username"
+            required
+          />
+          <span className="text-red-500 font-medium">Input Error</span>
         </div>
-        <button>Create account</button>
+        <button className="primary-btn h-10">Create account</button>
       </form>
-      <div />
+      <div className="w-full h-px bg-neutral-500" />
       <div>
-        <Link href="/sms">
-          <span>icon</span>
+        <Link
+          className="primary-btn flex h-10 items-center justify-center gap-2"
+          href="/sms"
+        >
+          <span>
+            <ChatBubbleOvalLeftEllipsisIcon className="size-6" />
+          </span>
           <span>Sign up with SMS</span>
         </Link>
       </div>
