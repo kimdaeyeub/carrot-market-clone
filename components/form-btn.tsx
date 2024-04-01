995ca59003afa5cb010c460e7e8d79661efb9b44
kimdaeyeub <kdy3915@naver.com>
2024-04-01 18:24:15 +0900

    form component 추가 및 스타일 수정

---
+++ components/form-btn.tsx
@@ -0,0 +1,16 @@
+interface FormBtnProps {
+  loading: boolean;
+  text: string;
+}
+const FormBtn = ({ loading, text }: FormBtnProps) => {
+  return (
+    <button
+      disabled={loading}
+      className="primary-btn h-10 disabled:bg-neutral-400 disabled:text-neutral-300 disabled:cursor-not-allowed"
+    >
+      {loading ? "로딩중..." : text}
+    </button>
+  );
+};
+
+export default FormBtn;
