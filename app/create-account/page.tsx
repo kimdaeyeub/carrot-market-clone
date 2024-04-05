e79a3e466445c81ff17748512652b7e9ac962c28
kimdaeyeub <kdy3915@naver.com>
2024-04-06 03:28:22 +0900

    Init zod

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,15 +1,19 @@
+"use client";
 import FormBtn from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
+import { useFormState } from "react-dom";
+import { createAccount } from "./actions";
 
 export default function CreateAccount() {
+  const [state, action] = useFormState(createAccount, null);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Fill in the form below to join!</h2>
       </div>
-      <form className="flex flex-col gap-3">
+      <form action={action} className="flex flex-col gap-3">
         <FormInput
           name="username"
           type="text"
@@ -32,7 +36,7 @@ export default function CreateAccount() {
           errors={[]}
         />
         <FormInput
-          name="password_confirm"
+          name="confirm_password"
           type="password"
           placeholder="Confirm Password"
           required
--- components/form-input.tsx
+++ components/form-input.tsx
@@ -2,14 +2,14 @@ interface FormInputProps {
   type: string;
   placeholder: string;
   required: boolean;
-  errors: string[];
+  errors?: string[];
   name: string;
 }
 const FormInput = ({
   type,
   placeholder,
   required,
-  errors,
+  errors = [],
   name,
 }: FormInputProps) => {
   return (
