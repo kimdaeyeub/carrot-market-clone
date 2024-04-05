642858835ec8a4e960c7281293479104a17b58fd
kimdaeyeub <kdy3915@naver.com>
2024-04-06 03:39:40 +0900

    Validation errors

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -19,28 +19,28 @@ export default function CreateAccount() {
           type="text"
           placeholder="Username"
           required
-          errors={[]}
+          errors={state?.fieldErrors.username}
         />
         <FormInput
           name="email"
           type="email"
           placeholder="Email"
           required
-          errors={[]}
+          errors={state?.fieldErrors.email}
         />
         <FormInput
           name="password"
           type="password"
           placeholder="Password"
           required
-          errors={[]}
+          errors={state?.fieldErrors.password}
         />
         <FormInput
           name="confirm_password"
           type="password"
           placeholder="Confirm Password"
           required
-          errors={[]}
+          errors={state?.fieldErrors.confirm_password}
         />
         <FormBtn text="Create Account" />
       </form>
