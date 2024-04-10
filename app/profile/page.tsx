74004ca1b540f93bae028b4acd38a771d10d8920
kimdaeyeub <kdy3915@naver.com>
2024-04-11 00:23:49 +0900

    log out

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,38 @@
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { notFound, redirect } from "next/navigation";
 import React from "react";
 
-const Profile = () => {
-  return <div>Profile</div>;
+const getUser = async () => {
+  const session = await getSession();
+  if (session.id) {
+    const user = await db.user.findUnique({
+      where: {
+        id: session.id,
+      },
+    });
+    if (user) {
+      return user;
+    }
+  }
+  notFound();
+};
+const Profile = async () => {
+  const user = await getUser();
+  const logOut = async () => {
+    "use server";
+    const session = await getSession();
+    await session.destroy();
+    redirect("/");
+  };
+  return (
+    <div>
+      <h1>Welcome! {user?.username}</h1>
+      <form action={logOut}>
+        <button>Logout</button>
+      </form>
+    </div>
+  );
 };
 
 export default Profile;
