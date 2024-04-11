fa789002058514bc0bd7ce7f50d1a266791f8dab
kimdaeyeub <kdy3915@naver.com>
2024-04-11 16:58:57 +0900

    Access token

---
+++ app/github/complete/route.ts
@@ -0,0 +1,30 @@
+import { notFound } from "next/navigation";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const code = request.nextUrl.searchParams.get("code");
+  if (!code) {
+    return notFound();
+  }
+  let accessTokenURL = "https://github.com/login/oauth/access_token";
+  const accessTokenParams = new URLSearchParams({
+    client_id: process.env.GITHUB_CLIENT_ID!,
+    client_secret: process.env.GITHUB_CLIENT_SECRET!,
+    code,
+  }).toString();
+  accessTokenURL = `${accessTokenURL}?${accessTokenParams}`;
+  const accessTokenResponse = await (
+    await fetch(accessTokenURL, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+  ).json();
+  if ("error" in accessTokenResponse) {
+    return new Response(null, {
+      status: 400,
+    });
+  }
+  return Response.json({ accessTokenResponse });
+}
