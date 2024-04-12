fbada18c7e355e38c4e1ba3fce2d8f1fab775d98
kimdaeyeub <kdy3915@naver.com>
2024-04-13 02:34:43 +0900

    Product Component

---
+++ components/list-product.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface ListProductProp {
+  title: string;
+  price: number;
+  created_at: Date;
+  photo: string;
+  id: number;
+}
+
+const ListProduct = ({
+  title,
+  price,
+  created_at,
+  photo,
+  id,
+}: ListProductProp) => {
+  return (
+    <Link href={`/products/${id}`} className="flex gap-5">
+      <div className="relative size-28 rounded-md overflow-hidden">
+        <Image fill src={photo} alt={title} />
+      </div>
+      <div className="flex flex-col gap-1 *:text-white">
+        <span className="text-lg">{title}</span>
+        <span className="text-sm text-neutral-500">
+          {created_at.toString()}
+        </span>
+        <span className="text-lg font-semibold">{price}</span>
+      </div>
+    </Link>
+  );
+};
+
+export default ListProduct;
