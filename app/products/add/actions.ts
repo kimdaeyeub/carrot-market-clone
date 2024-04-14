83ce87a37a8572fd36f8cb5a06187ff0f7c08178
kimdaeyeub <kdy3915@naver.com>
2024-04-14 22:07:51 +0900

    form action

---
+++ app/products/add/actions.ts
@@ -0,0 +1,11 @@
+"use server";
+
+export async function uploadProduct(formData: FormData) {
+  const data = {
+    title: formData.get("title"),
+    photo: formData.get("photo"),
+    price: formData.get("price"),
+    description: formData.get("description"),
+  };
+  console.log(data);
+}
