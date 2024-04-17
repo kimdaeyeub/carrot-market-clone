36f93c609d3e5c3f603f740d25370813f1dcd507
kimdaeyeub <kdy3915@naver.com>
2024-04-18 03:13:36 +0900

    Revert "cache"
    This reverts commit 112431323ab4a614166c671a710142227b553eda.

--- app/(tabs)/home/page.tsx
+++ app/(tabs)/home/page.tsx
@@ -3,10 +3,14 @@ import ProductList from "@/components/product-list";
 import db from "@/lib/db";
 import { PlusIcon } from "@heroicons/react/24/solid";
 import { Prisma } from "@prisma/client";
-import { revalidatePath, unstable_cache as nextCache } from "next/cache";
+import { unstable_cache } from "next/cache";
 import Link from "next/link";
 
-const getCachedProducts = nextCache(getInitialProducts, ["home-products"]);
+const getCachedProducts = unstable_cache(getInitialProducts, ["home-products"]);
+
+export const metadata = {
+  title: "Home",
+};
 
 async function getInitialProducts() {
   const products = await db.product.findMany({
@@ -28,28 +32,14 @@ export type InitialProducts = Prisma.PromiseReturnType<
   typeof getInitialProducts
 >;
 
-export const metadata = {
-  title: "Home",
-};
-
-//export const dynamic = "force-dynamic";
-// export const revalidate = 60;
-
 export default async function Products() {
-  const initialProducts = await getInitialProducts();
-  const revalidate = async () => {
-    "use server";
-    revalidatePath("/home");
-  };
+  const initialProducts = await getCachedProducts();
   return (
     <div>
       <ProductList initialProducts={initialProducts} />
-      <form action={revalidate}>
-        <button>Revalidate</button>
-      </form>
       <Link
         href="/products/add"
-        className="bg-orange-500 flex items-center justify-center rounded-full size-16 fixed bottom-24 right-8 text-white transition-colors hover:bg-orange-400"
+        className="bg-orange-500 flex justify-center items-center rounded-full size-16 fixed bottom-24 right-8 text-white transition-colors hover:bg-orange-400"
       >
         <PlusIcon className="size-10" />
       </Link>
